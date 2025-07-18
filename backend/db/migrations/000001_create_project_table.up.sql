d2be8887bb922056b8f9229d09a9e6d52f9468f4
lenardjombo <leonardjombo6@gmail.com>
2025-07-18 23:35:07 +0300

    file deletions

--- backend/db/migrations/000001_create_project_table.up.sql
+++
@@ -1,9 +0,0 @@
--- Enable UUID generation extension
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE project (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
-);
