7a3f28a1f7e8f87ba206b21055aaeaee44adf287
lenardjombo <leonardjombo6@gmail.com>
2025-07-21 20:59:57 +0300

    feat: updated users.sql

--- backend/db/queries/users.sql
+++ backend/db/queries/users.sql
@@ -20,11 +20,11 @@ SELECT * FROM users
 ORDER BY email;
 
 
--- name: UpdateAuthor :exec
+-- name: UpdateUser :exec
 UPDATE users
   set email = $2
 WHERE id = $1;
 
--- name: DeleteAuthor :exec
+-- name: DeleteUser :exec
 DELETE FROM users
 WHERE id = $1;
