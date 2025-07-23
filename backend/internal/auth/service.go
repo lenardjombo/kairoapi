635bbf45c822fd442f94b69d34a05a46208cb7db
lenardjombo <leonardjombo6@gmail.com>
2025-07-23 13:38:54 +0300

    feat(auth/service): created Abstraction layer(AuthService)

--- backend/internal/auth/service.go
+++ backend/internal/auth/service.go
@@ -1,1 +1,27 @@
 package auth
+
+import (
+	"context"
+	// "errors"
+	// "fmt"
+	// "time"
+
+	"github.com/google/uuid"
+	"github.com/lenardjombo/kairoapi/db/sqlc"
+	// "golang.org/x/crypto/bcrypt"
+)
+
+// AuthService defines the contract for user authentication and management.
+type AuthService interface {
+	RegisterUser(ctx context.Context, arg db.CreateUserParams) (db.User, error)
+	LoginUser(ctx context.Context, email, password string) (db.User, error)
+	GetUserById(ctx context.Context, id uuid.UUID) (db.User, error)
+	ListUsers(ctx context.Context) ([]db.User, error)
+	UpdateUser(ctx context.Context, id uuid.UUID, arg db.UpdateUserParams) error
+	DeleteUser(ctx context.Context, id uuid.UUID) error
+}
+
+// service implements AuthService
+type service struct {
+	repo UserRepository
+}
