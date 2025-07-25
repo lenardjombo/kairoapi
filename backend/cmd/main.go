a108e8f0cac85b12a4f9c03926aab694c15a04e7
lenardjombo <leonardjombo6@gmail.com>
2025-07-25 07:05:57 +0300

    feat: Set up gin server and initialize dependancies in main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/lenardjombo/kairoapi/db/sqlc"
+	"github.com/lenardjombo/kairoapi/internal/auth"
 	"github.com/lenardjombo/kairoapi/pkg"
+	"github.com/lenardjombo/kairoapi/routes"
 )
 
 func main() {
-	// 1. Initialize the DB connection
+	// 1. Init DB from pkg/db.go
 	if err := pkg.Init(); err != nil {
-		log.Fatalf("Failed to connect to the DB: %s", err)
+		log.Fatal("Failed to initialize database:", err)
 	}
 
-	// 2. Inform the user that the server is starting
-	fmt.Println("Server is running on http://127.0.0.1:8080")
+	// 2. Init SQLC Queries
+	queries := db.New(pkg.DB)
 
-	// 3. Start the HTTP server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Failed to start server: %s", err)
+	// 3. Init Repository → Service → Handler
+	userRepo := auth.NewUserRepository(queries)
+	authService := auth.NewAuthService(userRepo)
+	authHandler := auth.NewHandler(authService)
+
+	// 4. Settingup Gin & Auth Routes
+	router := gin.Default()
+	api := router.Group("/api")
+	routes.RegisterAuthRoutes(api, authHandler)
+
+	// 5. Starting Server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Server running on http://localhost:%s", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Server error:", err)
 	}
 }
